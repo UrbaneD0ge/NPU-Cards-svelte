65471da6c3c61cfce808432c8c2b12ed3c51a8b3
Kip <Kipling.Dunlap@gmail.com>
2023-03-26 12:10:52 -0400

    getting the hang of components

---
+++ src/components/Search.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+  <div>
+    <form class="col s12">
+      <div class="row m2">
+        <br>
+        <div class="input-field col s6">
+          <input id="address" type="text" class="">
+          <label for="Address">Address</label>
+          <button id="addySearch" class="btn green">Address Search</button>
+          <button id="geo" class="btn blue">🧭 Locate Me</button>
+          <span id="status" class="helper-text" data-error="Can't get your location.." data-success=""></span>
+        </div>
+      </div>
+    </form>
+
+    <div id='npuCard' class='row center-align' hidden>
+      <div class='col s3'>
+        <div class="card">
+          <div class="card-content">
+            <p>Your NPU is:</p>
+            <a id='npuLink'>
+              <h1 id='results'></h1>
+            </a>
+            <br>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+  import Search from '../components/Search.svelte';
+  import Email from '../components/Email.svelte';
+</script>
+
+<Search />
+<Email />
