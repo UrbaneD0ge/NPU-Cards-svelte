4b286dc023b7a55f40dd4103a713a24b218b7510
Kip <Kipling.Dunlap@gmail.com>
2023-03-26 13:15:40 -0400

    oohwhee learning Svelte

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,4 +1,50 @@
 <script>
+  let geoStatus = 'Click the 🧭 to find your location';
+function geoLocate() {
+  // Get the location of the user and put address in the input field
+  if (!navigator.geolocation) {
+    geoStatus = 'Geolocation is not supported by your browser';
+    return;
+  }
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    // const lat = document.querySelector('#lat');
+    // const lon = document.querySelector('#lon');
+    // lat.value = latitude;
+    // lon.value = longitude;
+    geoStatus = "Location found: " + latitude + ", " + longitude;
+    return latitude, longitude;
+  }
+  function error() {
+    geoStatus = 'Unable to retrieve your location';
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
+};
+
+function addySearch() {
+  // Get the location of the user and put address in the input field
+  let address = document.getElementById('address').value;
+  if (address == '') {
+    geoStatus = 'Please enter an address';
+    return;
+  }
+  address.urlEncoded = function () {
+    return encodeURIComponent(this);
+  };
+  fetch(`https://gis.atlantaga.gov/dpcd/rest/services/SiteAddressPoint/GeocodeServer/findAddressCandidates?Street=&Single+Line+Input=${address.urlEncoded}&maxLocations=1&matchOutOfRange=true=&f=pjson`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const latitude = data.candidates[0].location.y;
+      const longitude = data.candidates[0].location.x;
+      if (latitude && longitude) {
+        getNPU(latitude, longitude);
+      } else {
+        results.innerText = 'Not Found!?';
+      };
+    });
+};
 
 </script>
 
@@ -9,9 +55,9 @@
         <div class="input-field col s6">
           <input id="address" type="text" class="">
           <label for="Address">Address</label>
-          <button id="addySearch" class="btn green">Address Search</button>
-          <button id="geo" class="btn blue">🧭 Locate Me</button>
-          <span id="status" class="helper-text" data-error="Can't get your location.." data-success=""></span>
+          <button on:click={addySearch} class="btn green">Address Search</button>
+          <button on:click={geoLocate} class="btn blue">🧭 Locate Me</button>
+          <span id="geoStatus" class="helper-text" data-error="Can't get your location..">{geoStatus}</span>
         </div>
       </div>
     </form>
@@ -22,7 +68,7 @@
           <div class="card-content">
             <p>Your NPU is:</p>
             <a id='npuLink'>
-              <h1 id='results'></h1>
+              <h1 id='results'>results go here</h1>
             </a>
             <br>
           </div>
