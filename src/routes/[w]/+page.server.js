c6ffcae9ea167da5e44147d0807b951d631012c6
Kip <Kipling.Dunlap@gmail.com>
2023-04-01 20:45:44 -0400

    query isn't querying

--- src/routes/[w]/+page.server.js
+++ src/routes/[w]/+page.server.js
@@ -1,34 +1,14 @@
-import { error } from '@sveltejs/kit';
 import { Schema, model } from "mongoose";
-import { NPU } from './src/models/NPUs.js';
-import { start_mongo } from '$db/mongo';
+import { NPUs } from '$db/NPUs';
+// import { start_mongo } from '$db/mongo';
 import { mongoose } from 'mongoose';
 
 export async function load({ params }) {
   // find the NPU in the database corresponding to the URL parameter
-  const npu = await NPU.find({ NPU: req.query.NPU })
-
+  const data = await NPUs.findOne({ NPU: Request.params });
+  console.log(params, NPUs);
+  console.log('DB data: ' + data);
   return {
-    get: async ({ params }) => {
-
-      start_mongo().then(() => {
-        console.log('Connected to MongoDB!');
-      }).catch(err => {
-        console.log('Could not connect to the database. Exiting now...', err);
-        process.exit();
-      });
-
-      db.on('error', console.error.bind(console, 'connection error:'));
-      db.once('open', function () {
-        console.log('we are connected!');
-      }
-      );
-      NPU.find({ npu: req.query.NPU }, function (err, npu) {
-        if (err) return console.error(err);
-        console.log(npu);
-      }
-      );
-      return data;
-    }
-  }
+    NPUs: data
+  };
 };
--- src/routes/[w]/+page.svelte
+++ src/routes/[w]/+page.svelte
@@ -1,35 +1,36 @@
 <div class="row">
   <div class="col s12">
     <div class="card">
-      <h2>NPU-___</h2>
-      <!-- <div class="card-content">
-        <h5 class="card-title">NPU-{NPU.NPU}
+      <h2>NPU-{NPUs.npu}</h2>
+      <div class="card-content">
+        <h5 class="card-title">NPU-{NPUs.NPU}
         </h5>
         <p>Neighborhood Planning Unit: <strong>
-          {NPU.NPU}
+          {NPUs.NPU}
           </strong></p>
         <p>Chair: <strong>
-          {NPU.chair}
+          {NPUs.chair}
           </strong></p>
         <p>Meeting Frequency: <strong>
-            {NPU.meeting.split(',')[0]}
+            <!-- {NPUs.meeting.split(',')[0]} -->
           </strong></p>
         <p>Meeting Location: <strong>Virtual</strong></p>
         <p>Meeting Time: <strong>
-          {NPU.meeting.split(',')[1]}
+          <!-- {NPUs.meeting.split(',')[1]} -->
           </strong></p>
         <p>Meeting Location: <strong>physical location</strong></p>
       </div>
       <div class="card-action">
-        <a href="mailto:{NPU.chairE}">
-          {NPU.chairE}
+        <a href="mailto:{NPUs.chairE}">
+          {NPUs.chairE}
         </a><br>
-        <a href="{NPU.ZoomURL}" target='_blank' noopener noreferrer>Zoom Link</a>
-      </div> -->
+        <a href="{NPUs.ZoomURL}" target='_blank' noopener noreferrer>Zoom Link</a>
+      </div>
     </div>
   </div>
 </div>
 
 <script>
-
+  export let data
+$: NPUs = data
 </script>
