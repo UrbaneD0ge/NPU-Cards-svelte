551b51f4223614cc10d366fb38efbff9d3723837
Kip Dunlap <kipling.dunlap@gmail.com>
2023-04-03 13:51:24 -0400

    Something about Query

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,8 @@
-import { start_mongo } from '$db/mongo';
+// import { start_mongo } from '$db/mongo';
 
-start_mongo().then(() => {
-  console.log('Connected to MongoDB!');
-}).catch(err => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
-});
+// start_mongo().then(() => {
+//   console.log('Connected to MongoDB!');
+// }).catch(err => {
+//   console.log('Could not connect to the database. Exiting now...', err);
+//   process.exit();
+// });
--- src/routes/viewall/+page.server.js
+++ src/routes/viewall/+page.server.js
@@ -1,9 +1,18 @@
 import { Error } from "@sveltejs/kit";
 import { NPUs } from "$db/NPUs";
+import { start_mongo } from '$db/mongo';
+
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ props }) {
-  const units = await NPUs.find({}).toArray();
+  start_mongo().then(() => {
+    console.log('Connected to MongoDB!');
+  }).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+  });
+
+  const units = await NPUs.findOne({ NPU: "W" }).toArray();
   console.log(units);
   return {
     props: {
