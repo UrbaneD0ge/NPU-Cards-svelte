601f9b07dba4c03cffa5ac2cfe136a855b1e55cc
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2023-05-14 12:39:17 -0400

    I have completely forgotten how to format data

--- src/routes/[w]/+page.server.js
+++ src/routes/[w]/+page.server.js
@@ -6,7 +6,7 @@ import { mongoose } from 'mongoose';
 export async function load({ params }) {
   // find the NPU in the database corresponding to the URL parameter
   const data = await NPUs.findOne({ NPU: Request.params });
-  console.log(params, NPUs);
+  console.log('params' + params,'NPU from db:' + NPUs);
   console.log('DB data: ' + data);
   return {
     NPUs: data
