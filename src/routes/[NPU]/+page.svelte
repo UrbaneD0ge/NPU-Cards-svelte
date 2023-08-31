97405dde46f5ce55c7354222e374d9f4214ce875
Kip <Kipling.Dunlap@gmail.com>
2023-08-31 17:26:41 -0400

    script module to load data

--- src/routes/[NPU]/+page.svelte
+++ src/routes/[NPU]/+page.svelte
@@ -1,3 +1,20 @@
+<script context="module">
+  import { Schema, model } from "mongoose";
+import { NPUs } from '$db/NPUs';
+// import { start_mongo } from '$db/mongo';
+import { mongoose } from 'mongoose';
+
+export async function load({ params }) {
+  // find the NPU in the database corresponding to the URL parameter
+  const data = await NPUs.findOne({ NPU: Request.params });
+  console.log(params, NPUs);
+  console.log('DB data: ' + data);
+  return {
+    NPUs: data
+  };
+};
+</script>
+
 <div class="row">
   <div class="col s12">
     <div class="card">
