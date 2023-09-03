58bb55cae3da94b824a9f42a0a34cb230ea8b2c5
Kip <Kipling.Dunlap@gmail.com>
2023-09-03 16:35:13 -0400

    remove vite preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,7 @@
 import adapter from '@sveltejs/adapter-node';
-import {
-	vitePreprocess
-} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
