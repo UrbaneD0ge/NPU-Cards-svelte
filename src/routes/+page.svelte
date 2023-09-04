d5af5a52b3729bcf27347e164ca4def5b9d2da01
Kip <Kipling.Dunlap@gmail.com>
2023-09-04 14:23:26 -0400

    fighting the compiler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,12 @@
 <script>
   import Search from './Search.svelte';
-  import Map from './Map.svelte'
+  import Map from './Map.svelte';
   import Email from './Email.svelte';
 </script>
-
 
 <h1>NPU Locator</h1>
 <p>Search for your address below, or use your current location!</p>
 
-
 <Search />
-<Map />
-
-<!-- <NPU /> -->
-
+<!-- <Map /> -->
 <Email />
--- src/routes/Map.svelte
+++ src/routes/Map.svelte
@@ -1,4 +1,10 @@
-<div id="map"></div>
+<svelte:head>
+  <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDY9gzpWCprcXyaZ5IHXvTZGhAlenx-d4g&callback=initMap&libraries=maps,marker&v=beta"/>
+</svelte:head>
+
+<div id="map">
+    <gmp-map center="33.75,-84.34" zoom="10" map-id="DEMO_MAP_ID"></gmp-map>
+</div>
 
 <style>
   div {
@@ -12,42 +18,3 @@
     height: 400px
   }
 </style>
-
-<script>
-  import { onMount } from 'svelte';
-  // import { MAPS_API_KEY } from '$env/static/private';
-
-  onMount(() => {
-    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-
-    ({key: MAPS_API_KEY, v: "beta"});
-
-    let map;
-
-  async function initMap() {
-    // The location of Uluru
-    const position = { lat: 33.75, lng: -84.34 };
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-    // The map
-    map = new Map(document.getElementById("map"), {
-      zoom: 12,
-      center: position,
-      mapId: "DEMO_MAP_ID",
-    });
-
-    // The marker
-    const marker = new AdvancedMarkerElement({
-      map: map,
-      position: position,
-      title: "Uluru",
-    });
-  }
-
-  initMap();
-
-  });
-</script>
